363c841f0941649573ea6a691b8973c0e4c76384
Brittany Morris <brittmmendez@gmail.com>
2018-02-08 13:18:00 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -4,7 +4,7 @@ class SchoolClassesController < ApplicationController
   end
 
   def show
-    @school_class=SchoolClass.find(params[:id]
+    @school_class=SchoolClass.find(params[:id])
   end
 
   def new
